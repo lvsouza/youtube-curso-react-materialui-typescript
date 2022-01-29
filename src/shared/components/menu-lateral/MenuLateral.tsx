fa1a4b2571211d078c190565829a00c33e40080f
lvsouza <ls18017@gmail.com>
2022-01-29 07:39:03 -0300

    fix: ajustes nos textos dos temas

--- src/shared/components/menu-lateral/MenuLateral.tsx
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -2,7 +2,7 @@ import { Avatar, Divider, Drawer, Icon, List, ListItemButton, ListItemIcon, List
 import { useMatch, useNavigate, useResolvedPath } from 'react-router-dom';
 import { Box } from '@mui/system';
 
-import { useDrawerContext } from '../../contexts';
+import { useAppThemeContext, useDrawerContext } from '../../contexts';
 
 interface IListItemLinkProps {
   to: string;
@@ -38,6 +38,7 @@ export const MenuLateral: React.FC = ({ children }) => {
   const smDown = useMediaQuery(theme.breakpoints.down('sm'));
 
   const { isDrawerOpen, drawerOptions, toggleDrawerOpen } = useDrawerContext();
+  const { toggleTheme } = useAppThemeContext();
 
   return (
     <>
@@ -67,6 +68,16 @@ export const MenuLateral: React.FC = ({ children }) => {
             </List>
           </Box>
 
+          <Box>
+            <List component="nav">
+              <ListItemButton onClick={toggleTheme}>
+                <ListItemIcon>
+                  <Icon>dark_mode</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Alternar tema" />
+              </ListItemButton>
+            </List>
+          </Box>
         </Box>
       </Drawer>
 
--- src/shared/themes/Dark.ts
+++ src/shared/themes/Dark.ts
@@ -3,6 +3,7 @@ import { cyan, yellow } from '@mui/material/colors';
 
 export const DarkTheme = createTheme({
   palette: {
+    mode: 'dark',
     primary: {
       main: yellow[700],
       dark: yellow[800],
@@ -18,6 +19,11 @@ export const DarkTheme = createTheme({
     background: {
       paper: '#303134',
       default: '#202124',
+    },
+  },
+  typography: {
+    allVariants: {
+      color: 'white',
     }
   }
 });
