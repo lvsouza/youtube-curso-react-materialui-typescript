2efe23dee79052b30dc5a768a51e2f994044920e
lvsouza <ls18017@gmail.com>
2022-02-28 09:04:25 -0300

    feat: Adicionado service de pessoas com get all

---
+++ src/shared/services/api/pessoas/PessoasService.ts
@@ -0,0 +1,59 @@
+import { Environment } from '../../../environment';
+import { Api } from '../axios-config';
+
+
+interface IListagemPessoa {
+  id: number;
+  email: string;
+  cidadeId: number;
+  nomeCompleto: string;
+}
+
+interface IDetalhePessoa {
+  id: number;
+  email: string;
+  cidadeId: number;
+  nomeCompleto: string;
+}
+
+type TPessoasComTotalCount = {
+  data: IListagemPessoa[];
+  totalCount: number;
+}
+
+const getAll = async (page = 1, filter = ''): Promise<TPessoasComTotalCount | Error> => {
+  try {
+    const urlRelativa = `/pessoas?_page=${page}&_limit=${Environment.LIMITE_DE_LINHAS}&nomeCompleto_like=${filter}`;
+
+    const { data, headers } = await Api.get(urlRelativa);
+
+    if (data) {
+      return {
+        data,
+        totalCount: Number(headers['x-total-count'] || Environment.LIMITE_DE_LINHAS),
+      };
+    }
+
+    return new Error('Erro ao listar os registros.');
+  } catch (error) {
+    console.error(error);
+    return new Error((error as { message: string }).message || 'Erro ao listar os registros.');
+  }
+};
+
+const getById = async (): Promise<any> => { };
+
+const create = async (): Promise<any> => { };
+
+const updateById = async (): Promise<any> => { };
+
+const deleteById = async (): Promise<any> => { };
+
+
+export const PessoasService = {
+  getAll,
+  create,
+  getById,
+  updateById,
+  deleteById,
+};
