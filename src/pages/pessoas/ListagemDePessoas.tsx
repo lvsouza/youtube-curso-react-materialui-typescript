5ba24d963010fb06ba21bc605e29f4c6c45ee766
lvsouza <ls18017@gmail.com>
2022-03-12 08:34:04 -0300

    feat: Adicionado hook de useDebounce

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -4,10 +4,12 @@ import { useSearchParams } from 'react-router-dom';
 import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 import { FerramentasDaListagem } from '../../shared/components';
 import { LayoutBaseDePagina } from '../../shared/layouts';
+import { useDebounce } from '../../shared/hooks';
 
 
 export const ListagemDePessoas: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const { debounce } = useDebounce(3000, false);
 
   const busca = useMemo(() => {
     return searchParams.get('busca') || '';
@@ -16,14 +18,16 @@ export const ListagemDePessoas: React.FC = () => {
 
   useEffect(() => {
 
-    PessoasService.getAll(1, busca)
-      .then((result) => {
-        if (result instanceof Error) {
-          alert(result.message);
-        } else {
-          console.log(result);
-        }
-      });
+    debounce(() => {
+      PessoasService.getAll(1, busca)
+        .then((result) => {
+          if (result instanceof Error) {
+            alert(result.message);
+          } else {
+            console.log(result);
+          }
+        });
+    });
   }, [busca]);
 
 
---
+++ src/shared/hooks/UseDebounce.ts
@@ -0,0 +1,22 @@
+import { useCallback, useRef } from 'react';
+
+
+export const useDebounce = (delay = 300, notDelayInFirstTime = true) => {
+  const isFirstTime = useRef(notDelayInFirstTime);
+  const debouncing = useRef<NodeJS.Timeout>();
+
+
+  const debounce = useCallback((func: () => void) => {
+    if (isFirstTime.current) {
+      isFirstTime.current = false;
+      func();
+    } else {
+      if (debouncing.current) {
+        clearTimeout(debouncing.current);
+      }
+      debouncing.current = setTimeout(() => func(), delay);
+    }
+  }, [delay]);
+
+  return { debounce };
+};
