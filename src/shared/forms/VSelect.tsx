7359129d61e8475f4803d4f2cf8692f36424abaf
lvsouza <ls18017@gmail.com>
2022-07-04 12:57:22 -0300

    feat: added select component

---
+++ src/shared/forms/VSelect.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useState } from 'react';
+import { TextField, TextFieldProps } from '@mui/material';
+import { useField } from '@unform/core';
+
+
+type TVSelectProps = TextFieldProps & {
+  name: string;
+}
+export const VSelect: React.FC<TVSelectProps> = ({ name, ...rest }) => {
+  const { fieldName, registerField, defaultValue, error, clearError } = useField(name);
+
+  const [value, setValue] = useState(defaultValue || '');
+
+
+  useEffect(() => {
+    registerField({
+      name: fieldName,
+      getValue: () => value,
+      setValue: (_, newValue) => setValue(newValue),
+    });
+  }, [registerField, fieldName, value]);
+
+
+  return (
+    <TextField
+      {...rest}
+
+      select
+      error={!!error}
+      helperText={error}
+      defaultValue={defaultValue}
+
+      value={value}
+      onKeyDown={e => { error && clearError(); rest.onKeyDown?.(e); }}
+      onChange={e => { setValue(e.target.value); rest.onChange?.(e); }}
+    />
+  );
+};
--- src/shared/forms/index.ts
+++ src/shared/forms/index.ts
@@ -1,5 +1,6 @@
 export * from './IVFormErrors';
 export * from './VTextField';
 export * from './useVForm';
+export * from './VSelect';
 export * from './VScope';
 export * from './VForm';
