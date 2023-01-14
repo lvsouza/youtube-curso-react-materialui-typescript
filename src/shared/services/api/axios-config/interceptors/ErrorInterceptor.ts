b048e87f6c58861b238f93c53ab4abb1b8fd39d6
lvsouza <ls18017@gmail.com>
2023-01-14 08:46:37 -0300

    fix: fix bug in interceptor

--- src/shared/services/api/axios-config/interceptors/ErrorInterceptor.ts
+++ src/shared/services/api/axios-config/interceptors/ErrorInterceptor.ts
@@ -9,7 +9,12 @@ export const errorInterceptor = (error: AxiosError) => {
 
   if (error.response?.status === 401) {
     localStorage.removeItem('APP_ACCESS_TOKEN');
-    window.location.reload();
+
+    if (error.config.url?.includes('entrar')) {
+      alert(error.response.data.errors.default);
+    } else {
+      window.location.reload();
+    }
   }
 
   return Promise.reject(error);
